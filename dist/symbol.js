71d6d566887ff40eb65e39a6d753e0f78a8e4d62
Ba <ba@duongvanba.laptop.toshiba.c40>
2020-02-13 21:52:29 +0700

    Update

--- dist/symbol.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AMQP_QUEUES = Symbol.for('AMQP_QUEUES');
-exports.AMQP_DECORATER_INITED = Symbol.for('AMQP_DECORATER_INITED');
-exports.WAITFOR = Symbol.for('WAITFOR');
