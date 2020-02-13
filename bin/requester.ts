8c9350f9b4c5ad185072a7b9305d499071c42d60
Ba <ba@duongvanba.laptop.toshiba.c40>
2020-02-13 22:58:28 +0700

    Update

--- bin/requester.ts
+++
@@ -1,8 +0,0 @@
-import { AMQP, AmqpRemoteService } from '../src/AMQP'
-import { ServiceX } from './ServiceX'
-
-setImmediate(async () => {
-   await AMQP.init()
-   const service_x = await AmqpRemoteService<ServiceX>(ServiceX)
-   console.log(await service_x.some_method())//  =>  This is response from remote service
-})
--- bin/responder.ts
+++
@@ -1,7 +0,0 @@
-import {AMQP} from '../src/AMQP'
-import {ServiceX} from './ServiceX'
-
-setImmediate(async() => {
-    await AMQP.init()
-    new ServiceX()
-})
